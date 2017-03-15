db4d10416fc8d1ac5b93782733281a0e79839109
autumn31 <autumn.forever31@gmail.com>
2017-03-15 16:17:42 +0800

    done version one

--- contentscript.js
+++ contentscript.js
@@ -1,1 +1,7 @@
 console.log("Content JS...");
+if ($('header')){
+	$('header').append($('<div class="mylink"/>').append($('<img id="button" >').attr({'src':chrome.extension.getURL('tab.png')})));
+	var imgs = $('img');
+	console.log(imgs[imgs.length-1].getAttribute('src'));
+	$("#button").wrap('<a target="_blank" href="'+imgs[imgs.length-1].getAttribute('src')+'"></a>');
+}
